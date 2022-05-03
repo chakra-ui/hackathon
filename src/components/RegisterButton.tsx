763246053d031471839f515664d83d863c37522c
Lazar Nikolov <hello@nikolovlazar.com>
2022-05-03 07:51:13 +0200

    fixed register button

--- src/components/RegisterButton.tsx
+++ src/components/RegisterButton.tsx
@@ -2,10 +2,33 @@ import { Button, Flex } from '@chakra-ui/react';
 import * as React from 'react';
 
 export const RegisterButton = () => {
+  const canRegister = Date.now() >= new Date('2022-05-03').getTime();
+  const hasExpired = Date.now() >= new Date('2022-05-06').getTime();
+
+  if (hasExpired) return null;
+
+  if (!canRegister) {
+    return (
+      <Flex
+        display='inline-flex'
+        align='center'
+        px='4'
+        bg='teal.100'
+        color='teal.700'
+        rounded='md'
+        py='2'
+      >
+        <span>
+          Registration opens <b>May 3rd</b>
+        </span>
+      </Flex>
+    );
+  }
+
   return (
     <Button
       colorScheme='teal'
-      variant='solid'
+      variant={canRegister ? 'solid' : 'ghost'}
       as='a'
       href='https://docs.google.com/forms/d/1TxLVq0IWRkmCv4z8Yf5DYm89OxDl5WyWPAexgZRX1WU/edit'
       target='_blank'
